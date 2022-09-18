ca7135b1e1b6e5ca1b9fc41a33863ed17213bc60
samdav3 <105758614+samdav3@users.noreply.github.com>
2022-09-18 15:12:08 -0500

    Add files via upload

---
+++ chapter02/chapter02_madlibs.py
@@ -0,0 +1,26 @@
+# inputs for variables
+verb = input('Enter a verb')
+number = input('Enter a number')
+noun = input('Enter a noun')
+adjective = input('Enter an adjective')
+
+
+print("I loved you first: but afterwards your love")
+print("By Christina Rossetti \n")
+
+print(f"I loved you {number}: but afterwards you {verb}")
+print(f"Outsoaring mine, sang such a loftier {noun}")
+print(f"As drowned the {adjective} cooings of my {noun}.")
+print(f"Which owes the other most? my {verb} was long,")
+print(f"An yours one moment seemed to wax more {adjective};")
+print("I loved and guessed at you, you construed me")
+print("And loved me for what might or might not be")
+print("Nay, weights and measures do us both a wrong.")
+print(f"For verily {verb} knows not 'mine' or 'thine;'")
+print(f"With separate 'I' and 'thou' free {verb} has done,")
+print(f"For one is both and both are one in {verb}:")
+print(f"Rich {verb} knows nought of 'thing that is not mine;'")
+print(f"Both have the {noun} and both the {noun} thereof,")
+print(f"Both of us, of the {verb} which makes us {number}. \n")
+
+print("-for my beloved girlfriend, Katie. may our love be everlasting.")
