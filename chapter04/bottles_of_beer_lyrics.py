95d343463af303fdd024975f6041b48f857ab107
samdav3 <105758614+samdav3@users.noreply.github.com>
2022-09-28 16:49:03 -0500

    Add files via upload

---
+++ chapter04/bottles_of_beer_lyrics.py
@@ -0,0 +1,8 @@
+for lyric in ['3 bottles of beer on the wall', '3 bottles of beer', 'Take one down, pass it around', '2 bottles of beer on the wall']:
+    print(lyric)
+print('\n')
+for lyric in ['2 bottles of beer on the wall', '2 bottles of beer', 'Take one down, pass it around', '1 bottle of beer on the wall']:
+    print(lyric)
+print('\n')
+for lyric in ['1 bottle of beer on the wall', '1 bottle of beer', 'Take one down, pass it around', '0 bottles of beer on the wall']:
+    print(lyric)
