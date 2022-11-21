249b883d0d43a2955aa84a28aeafa461ce5582fd
samdav3 <105758614+samdav3@users.noreply.github.com>
2022-11-21 11:15:41 -0600

    Add files via upload

---
+++ chatper11/ch_11_exercises.py
@@ -0,0 +1,51 @@
+"""
+    Starting Out with Python by Tony Gaddis, fifth edition
+    Complete all of the TODO directions
+    The number next to the TODO represents the chapter
+    and section in your textbook that explain the required code
+    Your file should compile error free (green checkmark upper right)
+    Submit your completed file
+"""
+
+# TODO 11.1 Introduction to Inheritance
+print("=" * 10, "Section 11.1 inheritance", "=" * 10)
+# You are going to create a Dwelling class based on the
+# Automobile sample from the chapter
+
+# 1) Create the class Dwelling, the __init__ method should accept number_of_rooms, square_feet, floors
+
+
+# 2) Add mutators for all of the data attributes (number_of_rooms, square_feet, floors)
+
+
+# 3) Add accessors for all of the data attributes
+
+
+# 4) Create the class SingleFamilyHome as a sub-class of Dwelling
+# The __init__ method should accept number_of_rooms, square_feet, floors, garage_type, yard_size
+# -- Call the __init__ of superclass Dwelling and pass the required arguments, remember to include self
+# -- Initialize the garage_type and yard_size attributes
+
+# 5) Create the mutator and accessor methods for the garage_type and yard_size attributes
+
+
+# Demonstrate the SingleFamilyHome class, no need to import because you are in the same file
+# 6) Create a main function.
+# 7) In main, create an object from the Single_family_home class with the following information:
+#            6 rooms, 1200 square feet, 1 floor, single car garage, .25 acres
+
+# 8) Display the data using the accessor methods
+
+# 9) Call the main function
+
+
+# TODO 11.2 Polymorphism
+print("=" * 10, "Section 11.2 polymorphism", "=" * 10)
+# 1) Type in the mammal class from program 11-9, lines 1 - 22
+
+# 2) Create a Mouse class as a sub-class of the mammal class following the Dog example
+
+# 3) Create a Sheep class as a sub-class of the mammal class following the Cat Example
+
+# 4) Follow the example in program 11-10 (no need to import, use main2 instead of main
+#    because there is already a main on this page) use the Mouse and Sheep class that you created
